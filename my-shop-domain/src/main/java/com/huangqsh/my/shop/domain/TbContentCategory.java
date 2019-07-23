97e27150ef808ca1a368986c916a88d6fc5c8e2e
huangqishan <1587083931@qq.com>
2019-07-23 23:15:47 +0800

    TbcontentCategory实体类

---
+++ my-shop-domain/src/main/java/com/huangqsh/my/shop/domain/TbContentCategory.java
@@ -0,0 +1,52 @@
+package com.huangqsh.my.shop.domain;
+
+import com.huangqsh.my.shop.commons.persistence.BaseEntity;
+
+public class TbContentCategory extends BaseEntity {
+
+    private Long parentId;
+    private String name;
+    private Integer status;
+    private Integer sortOrder;
+    private Boolean isParent;
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Boolean getParent() {
+        return isParent;
+    }
+
+    public void setParent(Boolean parent) {
+        isParent = parent;
+    }
+}
